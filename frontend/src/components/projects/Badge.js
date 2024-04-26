08ccb32ada3527f17c24070e157cef0365cbb6bf
Kia <kiasambrook@btinternet.com>
2024-04-26 15:02:13 +0100

    refactor: badge tidying up

--- frontend/src/components/projects/Badge.js
+++ frontend/src/components/projects/Badge.js
@@ -2,21 +2,11 @@ import React from "react";
 
 const Badge = ({ colour, text }) => {
   return (
-    <div className="tags bottom-0  transition-transform ease-in-out group-hover:bottom-10">
-      <span
-        className={
-          "badge inline-flex items-center rounded-md bg-" +
-          colour +
-          "-200 px-2 py-1 text-xs font-medium text-" +
-          colour +
-          "-700 ring-1 ring-inset ring-" +
-          colour +
-          "-700/10"
-        }
-      >
-        {text}
-      </span>
-    </div>
+    <span
+      className={`badge inline-flex items-center rounded-md bg-${colour}-200 px-2 py-1 text-xs font-medium text-${colour}-700 `}
+    >
+      {text}
+    </span>
   );
 };
 
--- frontend/src/components/projects/Project.js
+++ frontend/src/components/projects/Project.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Badge from "./Badge";
 
 const Project = ({ title, img, badges }) => {
@@ -9,12 +8,11 @@ const Project = ({ title, img, badges }) => {
         <div className="absolute inset-0 flex items-center justify-center mix-blend-soft-light hover:mix-blend-normal">
           <h2 className=" text-3xl font-bold">{title}</h2>
         </div>
-      </div>
-      <div className="tags flex gap-2 transition-transform ease-in-out group-hover:bottom-10">
-        {badges.map((badge, index) => (
+        <div className="tags flex gap-2">
+          {badges.map((badge, index) => (
             <Badge key={index} colour={badge.colour} text={badge.text} />
-          )
-        )}
+          ))}
+        </div>
       </div>
     </div>
   );
